5d041c346b57b466905d441f8723277e41eac1e9
melikesoygullucu <melikesoygul@hotmail.com>
2023-10-05 21:22:23 +0300

    Initial Commit

---
+++ Weather App/Weather_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_AppApp.swift
+//  Weather App
+//
+//  Created by Melike Soygüllücü on 5.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct Weather_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
