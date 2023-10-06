bbcd0639d0b1fdd8e07799c47f54cad018a76b53
melikesoygullucu <melikesoygul@hotmail.com>
2023-10-06 13:10:46 +0300

    welcome view

---
+++ Weather App/Views/WelcomeView.swift
@@ -0,0 +1,18 @@
+//
+//  WelcomeView.swift
+//  Weather App
+//
+//  Created by Melike Soygüllücü on 6.10.2023.
+//
+
+import SwiftUI
+
+struct WelcomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WelcomeView()
+}
