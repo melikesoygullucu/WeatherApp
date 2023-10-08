bc1ac5dae2ab4336897f589d7fb4961ad3a0dd7f
melikesoygullucu <melikesoygul@hotmail.com>
2023-10-08 22:57:52 +0300

    api call added.

---
+++ Weather App/Managers/WeatherManager.swift
@@ -0,0 +1,8 @@
+//
+//  WeatherManager.swift
+//  Weather App
+//
+//  Created by Melike Soygüllücü on 8.10.2023.
+//
+
+import Foundation
---
+++ Weather App/Views/LoadingView.swift
@@ -0,0 +1,18 @@
+//
+//  LoadingView.swift
+//  Weather App
+//
+//  Created by Melike Soygüllücü on 6.10.2023.
+//
+
+import SwiftUI
+
+struct LoadingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoadingView()
+}
